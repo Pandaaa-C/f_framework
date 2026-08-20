c21a16d5e60f2fc9f3105f2aa82219e6a71ff94d
Panda der echte <49002481+Pandaaa-C@users.noreply.github.com>
2026-08-20 10:00:57 +0100

    add: basic client framework

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,21 +1,44 @@
 /// <reference types="@citizenfx/client" />
+import {Vector3} from "../shared";
+
 export * from '../shared/index.js';
 
-export const Player = {
+export const player = {
     get ped(): number {
         return PlayerPedId();
     },
-    get position(): number[] {
-        return GetEntityCoords(PlayerPedId(), true);
+    get serverId(): number {
+        return GetPlayerServerId(PlayerId());
     },
-    set position(value: number[]) {
-        SetEntityCoords(PlayerPedId(), value[0], value[1], value[2], false, false, false, true);
+    get position(): Vector3 {
+        return Vector3.from(GetEntityCoords(PlayerPedId(), true));
+    },
+    set position(value: Vector3) {
+        SetEntityCoords(PlayerPedId(), value.x, value.y, value.z, false, false, false, true);
+    },
+    get health(): number {
+        return GetEntityHealth(PlayerPedId());
+    },
+    set health(value: number) {
+        SetEntityHealth(PlayerPedId(), value);
     }
-}
+};
 
-export const Client = {
+export const events = {
+    on: (event: string, handler: (...args: any[]) => void) => on(event, handler),
+    emit: (event: string, ...args: any[]) => emit(event, ...args),
+
     onNet: (event: string, handler: (...args: any[]) => void) => onNet(event, handler),
     emitNet: (event: string, ...args: any[]) => emitNet(event, ...args),
-    command: (name: string, handler: (src: number, args: string[]) => void) =>
+
+    addCommand: (name: string, handler: (src: number, args: string[]) => void) =>
         RegisterCommand(name, (source: number, args: string[]) => handler(source, args), false)
 };
+
+const keys = {
+    bind(command: string, description: string, key: string) {
+        RegisterKeyMapping(command, description, 'keyboard', key);
+    }
+};
+
+export const fx = {events, player, keys, Vector3};
