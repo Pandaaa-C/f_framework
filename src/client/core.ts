5d6cd587eea95cdb7c8695158c9f15690715ec19
Panda der echte <49002481+Pandaaa-C@users.noreply.github.com>
2026-09-06 10:02:58 +0100

    edit: register blip manager

--- src/client/core.ts
+++ src/client/core.ts
@@ -4,12 +4,14 @@ import {LocalPlayer} from "./entities/local-player";
 import {Vector3} from "../shared";
 import {registerInternalHandlers} from "./internal";
 import {registerBlipHandlers} from "./blips-internal";
+import {BlipManager} from "../server/blips";
 
 export class Core {
     readonly events = new EventManager();
     readonly commands = new CommandManager();
     readonly player = new LocalPlayer();
     readonly Vector3 = Vector3;
+    readonly blips = new BlipManager();
 
     constructor() {
         registerInternalHandlers();
--- src/server/core.ts
+++ src/server/core.ts
@@ -1,9 +1,11 @@
 import {EventManager} from "./managers/event-manager";
 import {PlayerPool} from "./managers/player-pool";
 import {Vector3} from "../shared";
+import {BlipManager} from "./blips";
 
 export class Core {
     readonly events = new EventManager();
     readonly players = new PlayerPool();
     readonly Vector3 = Vector3;
+    readonly blips = new BlipManager();
 }
